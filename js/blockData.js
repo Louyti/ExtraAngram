9a7c9e8480b2aed2b439ffb7d89e4a9189ea92fe
Louyti <lorensytas@gmail.com>
2019-09-11 13:48:44 +0300

    Angram gen v1

---
+++ js/blockData.js
@@ -0,0 +1,811 @@
+const blockData = {
+  letters: {
+    a: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '175',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    ā: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '175',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.8',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    b: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '180',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    c: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    č: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '45',
+        align: '54',
+        scx: '1',
+        scy: '0.4'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '-45',
+        align: '-54',
+        scx: '1',
+        scy: '0.4'
+      }
+    ],
+    d: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '175',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    e: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0.19',
+        rotation: '90',
+        align: '-8',
+        flip: 'false',
+        scx: '0.835',
+        scy: '0.835'
+      }
+    ],
+    ē: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0.19',
+        rotation: '90',
+        align: '-8',
+        flip: 'false',
+        scx: '0.835',
+        scy: '0.835'
+      },
+      {
+        type: 'i',
+        level: '0.8',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    f: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0.6',
+        rotation: '0',
+        align: '96',
+        flip: 'true',
+        scx: '0.835',
+        scy: '0.835'
+      },
+      {
+        type: 'i',
+        level: '0.4',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    g: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '-0.8',
+        rotation: '180',
+        align: '79',
+        flip: 'true',
+        scx: '0.835',
+        scy: '0.835'
+      }
+    ],
+    ģ: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '-0.8',
+        rotation: '180',
+        align: '79',
+        flip: 'true',
+        scx: '0.835',
+        scy: '0.835'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '45',
+        align: '54',
+        scx: '1',
+        scy: '0.4'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '-45',
+        align: '-54',
+        scx: '1',
+        scy: '0.4'
+      }
+    ],
+    h: [
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    i: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'dot',
+        level: '0.4',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    ī: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.8',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    j: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '-0.8',
+        rotation: '180',
+        align: '-96',
+        flip: 'true',
+        scx: '0.835',
+        scy: '0.835'
+      },
+      {
+        type: 'dot',
+        level: '0.4',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    k: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.06',
+        rotation: '30',
+        align: '-55',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-0.3',
+        rotation: '135',
+        align: '-10',
+        scx: '1',
+        scy: '0.5'
+      }
+    ],
+    ķ: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-175',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.06',
+        rotation: '30',
+        align: '-55',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-0.3',
+        rotation: '135',
+        align: '-10',
+        scx: '1',
+        scy: '0.5'
+      },
+      {
+        type: 'i',
+        level: '-0.7',
+        rotation: '25',
+        align: '-65',
+        scx: '1',
+        scy: '0.3'
+      }
+    ],
+    l: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '0.5'
+      }
+    ],
+    ļ: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '0.5'
+      },
+      {
+        type: 'i',
+        level: '-0.7',
+        rotation: '25',
+        align: '-65',
+        scx: '1',
+        scy: '0.3'
+      }
+    ],
+    m: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-200',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '-100',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '100',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    n: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '20',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    ņ: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '20',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-0.7',
+        rotation: '25',
+        align: '-65',
+        scx: '1',
+        scy: '0.3'
+      }
+    ],
+    o: [
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '-30',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'c',
+        level: '0',
+        rotation: '0',
+        align: '30',
+        flip: 'true',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    p: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '-150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-1',
+        rotation: '0',
+        align: '-150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '20',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    r: [
+      {
+        type: 'h',
+        level: '0',
+        rotation: '0',
+        align: '20',
+        flip: 'true',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    s: [
+      {
+        type: 's',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    š: [
+      {
+        type: 's',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '45',
+        align: '54',
+        scx: '1',
+        scy: '0.4'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '-45',
+        align: '-54',
+        scx: '1',
+        scy: '0.4'
+      }
+    ],
+    t: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.75',
+        rotation: '0',
+        align: '0',
+        scx: '1',
+        scy: '0.5'
+      },
+      {
+        type: 'i',
+        level: '0.4',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    u: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '180',
+        align: '-20',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      }
+    ],
+    ū: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '0',
+        align: '150',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'h',
+        level: '0',
+        rotation: '180',
+        align: '-20',
+        flip: 'false',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '0.8',
+        rotation: '90',
+        align: '0',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    v: [
+      {
+        type: 'i',
+        level: '0',
+        rotation: '20',
+        align: '100',
+        scx: '1',
+        scy: '1.1'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '-20',
+        align: '-100',
+        scx: '1',
+        scy: '1.1'
+      }
+    ],
+    z: [
+      {
+        type: 'i',
+        level: '0.45',
+        rotation: '90',
+        align: '-15',
+        scx: '1',
+        scy: '0.6'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '31',
+        align: '-15',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-0.45',
+        rotation: '90',
+        align: '-15',
+        scx: '1',
+        scy: '0.6'
+      }
+    ],
+    ž: [
+      {
+        type: 'i',
+        level: '0.45',
+        rotation: '90',
+        align: '-15',
+        scx: '1',
+        scy: '0.6'
+      },
+      {
+        type: 'i',
+        level: '0',
+        rotation: '31',
+        align: '-15',
+        scx: '1',
+        scy: '1'
+      },
+      {
+        type: 'i',
+        level: '-0.45',
+        rotation: '90',
+        align: '-15',
+        scx: '1',
+        scy: '0.6'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '45',
+        align: '54',
+        scx: '1',
+        scy: '0.4'
+      },
+      {
+        type: 'i',
+        level: '0.78',
+        rotation: '-45',
+        align: '-54',
+        scx: '1',
+        scy: '0.4'
+      }
+    ]
+  }
+}
--- js/draw.js
+++ js/draw.js
@@ -1,1 +1,239 @@
-alert();
+const ctx = plot.getContext("2d");
+
+plot.width = document.body.clientWidth;
+plot.height = document.body.clientHeight;
+
+let images = {
+  c: {
+    pic: new Image(),
+    left: 50
+  },
+  h: {
+    pic: new Image(),
+    left: 105
+  },
+  i: {
+    pic: new Image(),
+    left: 225
+  },
+  dot: {
+    pic: new Image(),
+    left: 200
+  },
+  s: {
+    pic: new Image(),
+    left: 45
+  }
+}
+
+var scale = 0.5;
+
+images.dot.pic.src = "images/rsz_dot.png";
+images.i.pic.src = "images/rsz_i.png";
+images.h.pic.src = "images/rsz_h.png";
+images.c.pic.src = "images/rsz_c.png";
+images.s.pic.src = "images/s.png";
+
+let inputString = "";
+let outputString = "";
+
+var animating = false;
+
+var movers;
+
+function display(string) {
+  if(!animating){
+    ctx.clearRect(0, 0, plot.width, plot.height);
+    for (i = 0; i < string.length; i++) {
+      if (blockData.letters[string[i]]) {
+        let segments = blockData.letters[string[i]];
+        for (o of segments) {
+          let image = images[o.type];
+          let rx = (380 + i * 500 + Number(o.align)) * scale;
+          let ry = (900 + 500 * -Number(o.level)) * scale;
+          let scx = Number(o.scx);
+          let scy = Number(o.scy);
+          if (o.flip && o.flip == "true") scx *= -1;
+          ctx.translate(rx, ry);
+          ctx.rotate(Math.PI / 180 * Number(o.rotation));
+          ctx.scale(scx * scale, scy * scale);
+          ctx.drawImage(image.pic, -((500 - image.left * 2) / 2), -250);
+          ctx.scale(1 / (scx * scale), 1 / (scy * scale));
+          ctx.rotate(-Math.PI / 180 * Number(o.rotation))
+          ctx.translate(-rx, -ry);
+        }
+      }
+    }
+  }
+}
+
+function getValue(string) {
+  let ret = {
+    c: 0,
+    h: 0,
+    i: 0,
+    dot: 0,
+    s: 0
+  };
+  for (i of string) {
+    let segments = blockData.letters[i];
+    try{
+      for (o of segments) {
+        ret[o.type]++;
+      }
+    } catch (e) {
+      //console.log(string);
+    }
+  }
+  return ret;
+}
+
+function prepare(string) {
+  let ret = {
+    c: [],
+    h: [],
+    i: [],
+    dot: [],
+    s: []
+  };
+  for (i = 0; i < string.length; i++) {
+    let segments = blockData.letters[string[i]];
+    for (o of segments) {
+      let rx = (380 + i * 500 + Number(o.align)) * scale;
+      let ry = (900 + 500 * -Number(o.level)) * scale;
+      let scx = Number(o.scx);
+      let scy = Number(o.scy);
+      if (o.flip && o.flip == "true") scx *= -1;
+      ret[o.type].push({
+        x: rx,
+        y: ry,
+        rot: Number(o.rotation),
+        sx: scx * scale,
+        sy: scy * scale
+      });
+    }
+  }
+  return ret;
+}
+
+function launchAnimation(str1, str2, time) {
+  animating = true;
+  document.getElementById("in").blur();
+  display(str1);
+  movers = prepare(str1);
+  var targets = prepare(str2);
+  for (i in movers) {
+    for (o = 0; o < movers[i].length; o++) {
+      let mo = movers[i][o];
+      let to = targets[i][o];
+      mo.xkey = (to.x - mo.x) / time;
+      mo.ykey = (to.y - mo.y) / time;
+      mo.rotkey = (to.rot - mo.rot) / time;
+      mo.sxkey = (to.sx - mo.sx) / time;
+      mo.sykey = (to.sy - mo.sy) / time;
+    }
+    //console.log(movers[i]);
+  }
+  drawAnimation(0, time);
+}
+
+function drawAnimation(key, time) {
+  ctx.clearRect(0, 0, plot.width, plot.height);
+  for (i in movers) {
+    for (o of movers[i]) {
+      o.x += o.xkey;
+      o.y += o.ykey;
+      o.rot += o.rotkey;
+      o.sx += o.sxkey;
+      o.sy += o.sykey;
+      let image = images[i];
+      ctx.translate(o.x, o.y);
+      ctx.rotate(Math.PI / 180 * Number(o.rot));
+      ctx.scale(o.sx, o.sy);
+      ctx.drawImage(image.pic, -((500 - image.left * 2) / 2), -250);
+      ctx.scale(1 / o.sx, 1 / o.sy);
+      ctx.rotate(-Math.PI / 180 * Number(o.rot))
+      ctx.translate(-o.x, -o.y);
+    }
+  }
+  if (key < time) {
+    setTimeout(() => drawAnimation(key + 1, time), 20);
+  } else {
+    finishAnimation();
+  }
+}
+
+function findwords(str1) {
+  let ret = [];
+  for (vards of vardi) {
+    let tvards = vards.toLowerCase();
+    if (compareWords(str1, tvards)) ret.push(tvards);
+  }
+  return ret;
+}
+
+function findword(str1) {
+  for (vards of vardi) {
+    let tvards = vards.toLowerCase();
+    if (compareWords(str1, tvards)) return tvards;
+  }
+  return null;
+}
+
+function compareWords(val1, val2) {
+  let get1 = getValue(val1);
+  let get2 = getValue(val2);
+  for (i in get1) {
+    if (get1[i] !== get2[i]) return false;
+  }
+  return true;
+}
+
+function transform() {
+  if(!animating){
+    let words = findwords(inputString);
+    document.getElementById("iespejamovardudaudzums").innerHTML = words.length;
+    document.getElementById("dati").innerHTML = JSON.stringify(getValue(inputString));
+    if(words.length > 0){
+      outputString = words[Math.floor(Math.random() * words.length)];
+      if(words.length > 1){
+        while(outputString == inputString){
+          outputString = words[Math.floor(Math.random() * words.length)];
+        }
+      }
+      if (outputString) {
+        launchAnimation(inputString, outputString, atrums.value);
+      }
+    } else {
+      alert("Šim vārdam nav līdzīgo!");
+    }
+  }
+}
+
+function finishAnimation() {
+  inputString = outputString;
+  animating = false;
+  document.getElementById("in").focus();
+  document.getElementById('in').value = inputString;
+  checkScreen();
+}
+
+function checkScreen() {
+  inputString = document.getElementById('in').value.toLowerCase();
+  if ((380 + inputString.length * 500) * scale > document.body.clientWidth - 10) {
+    scale *= 0.93;
+  }
+  display(inputString);
+}
+
+const node = document.getElementById('in');
+node.addEventListener("keyup", (event) => {
+  if (event.code == "Enter") {
+      transform();
+  }
+});
+
+images.s.pic.onload = () => {
+  display(inputString);
+  //launchAnimation(inputString, outputString, 100);
+};
